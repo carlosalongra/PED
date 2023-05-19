f50ce0ec07f222d0928043be0ac54af14be75475
carlosalongra <carlosalongra@gmail.com>
2023-05-19 11:15:42 +0200

    añadido hora sin test serv/cli

---
+++ testServidor/tiempo/socket_servidor.py
@@ -0,0 +1,36 @@
+import socket, time, os
+
+sok = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+path_sok = input("Introducir socket: ")
+
+if not path_sok:
+    path_sok = 'socket_prac4'
+if os.path.exists('/tmp/' + path_sok):
+    os.remove('/tmp/' + path_sok)
+
+sok.bind('/tmp/' + path_sok)
+
+horaformat = '%H:%M:%S'
+dateformat = '%m-%d-%Y'
+
+
+while True:
+    sok.listen(5)
+    conexion, addr = sok.accept()
+
+    mensaje = conexion.recv(1024)
+    
+    try:
+        hora = time.strftime(horaformat)
+        fecha = time.strftime(dateformat)
+        if mensaje.decode('utf8') == 'hora':
+            conexion.send(hora.encode('utf8'))
+
+        elif mensaje.decode('utf8') == 'fecha':
+            conexion.send(fecha.encode('utf8'))
+        
+        else: 
+            print('ERROR')
+    except:
+        conexion.send('consulta no existe'.encode('utf8'))
+    conexion.close()
